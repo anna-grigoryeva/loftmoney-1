2d7032acd26f555d9c909ab88a61d4391e15096a
Grigorieva <grigorieva-av@mail.ru>
2020-07-16 18:49:43 +0300

    init repository

---
+++ app/src/main/java/com/grigorieva/loftmoney/MainActivity.java
@@ -0,0 +1,25 @@
+package com.grigorieva.loftmoney;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newActivity = new Intent(getApplicationContext(), SecondActivity.class);
+                newActivity.putExtra("COST_ID", 1);
+                startActivity(newActivity);
+            }
+        });
+    }
+}
